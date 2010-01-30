8cec50725fb5e7d7581bf2c9e2731ccc1a7ffcf6
unknown <esquivel@.(none)>
2010-01-30 17:15:28 +0100

    Added player panel class.

---
+++ Honeymoon/PlayerPanel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace Honeymoon
+{
+    public class PlayerPanel : DrawableGameComponent
+    {
+        public Monkey Player;
+        public HoneymoonGame GameHM;
+        public Vector2 Position;
+
+        public PlayerPanel(Monkey player)
+            : base(HoneymoonGame.Instance)
+        {
+            this.GameHM = HoneymoonGame.Instance;
+            this.Player = player;
+            this.Position = Vector2.Zero;
+            Game.Components.Add(this);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            GameHM.CurrentTheme.Panel.Draw(Player.HitPoints % 5, Position, Color.White, 0.0f, 1.0f);
+        }
+    }
+}
