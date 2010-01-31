b96fd8e22ee4de1451cc7052d11c55ebecd6ca85
unknown <esquivel@.(none)>
2010-01-31 03:49:52 +0100

    Added noise sounds for evil world

--- Honeymoon/Game.cs
+++ Honeymoon/Game.cs
@@ -39,6 +39,7 @@ namespace Honeymoon
         public Intro IntroController;
         
         public SoundEffect WalkingSound;
+        public SoundEffectInstance NoiseSound;
         public Song GameOverMusic;
 
         public Theme CurrentTheme
@@ -115,6 +116,8 @@ namespace Honeymoon
             twitchRenderTarget = new RenderTarget2D(GraphicsDevice, 128, 128, 1, GraphicsDevice.DisplayMode.Format, RenderTargetUsage.PreserveContents);
 
             WalkingSound = Content.Load<SoundEffect>("Sounds/footsteps");
+            NoiseSound = Content.Load<SoundEffect>("Sounds/noise0").CreateInstance();
+            NoiseSound.IsLooped = true;
             GameOverMusic = Content.Load<Song>("Music/gameover");
 
             IntroController.Screen = Content.Load<Texture2D>("Textures/Backgrounds/title");
@@ -202,11 +205,29 @@ namespace Honeymoon
             {
                 themeTransition = (float)Math.Max(themeTransition - worldTransitionDiff, targetTheme);
                 twitchValue = themeTransition + 0.5f;
+                if (GameState == GameStates.Game)
+                {
+                    MediaPlayer.Volume = 1.0f - 0.75f * themeTransition;
+                    NoiseSound.Volume = 0.67f * themeTransition;
+                    if (NoiseSound.State == SoundState.Paused)
+                        NoiseSound.Resume();
+                    else if (NoiseSound.State == SoundState.Stopped)
+                        NoiseSound.Play();
+                }
             }
             else if(targetTheme > themeTransition)
             {
                 themeTransition = (float)Math.Min(themeTransition + worldTransitionDiff, targetTheme);
                 twitchValue = themeTransition + 0.5f;
+                if (GameState == GameStates.Game)
+                {
+                    MediaPlayer.Volume = 1.0f - 0.75f * themeTransition;
+                    NoiseSound.Volume = 0.67f * themeTransition;
+                    if (NoiseSound.State == SoundState.Paused)
+                        NoiseSound.Resume();
+                    else if (NoiseSound.State == SoundState.Stopped)
+                        NoiseSound.Play();
+                }
             }
             else if (GameState == GameStates.GameOver)
             {
@@ -228,6 +249,10 @@ namespace Honeymoon
                         changeTwitchValueGameOver = 0;
                     }
                 }
+            }
+            else if (themeTransition == 0.0f && NoiseSound.State == SoundState.Playing)
+            {
+                NoiseSound.Pause();
             }
 
             if (GameState == GameStates.Game)
@@ -338,8 +363,7 @@ namespace Honeymoon
             twitchEffect.Parameters["noise_tex_Tex"].SetValue(twitchNoise);
 
             EffectTechnique t = twitchEffect.Techniques["Screen_AlignedQuad"];
-            GraphicsDevice.ResolveBackBuffer(resolvedBackbuffer);
-            
+            GraphicsDevice.ResolveBackBuffer(resolvedBackbuffer);            
 
             // Pass 1
             spriteBatch.Begin(SpriteBlendMode.None, SpriteSortMode.Immediate, SaveStateMode.SaveState);
